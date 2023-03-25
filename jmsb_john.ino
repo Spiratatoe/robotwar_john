6215672f20a573419ef4d7d3aee89e1c560688e3
jack <95253269+Spiratatoe@users.noreply.github.com>
2023-03-25 19:42:23 -0400

    all pieces working
    tested each piece and gates, code works
    next step is to work on the robot logic
    since it will run on its own

--- jmsb_john.ino
+++ jmsb_john.ino
@@ -5,10 +5,24 @@
 //values are the pins in ard...
 const int motorA_1 = 1; //in1 
 const int motorA_2 = 2; //in2 
-const int motorA_en = 3;
+const int motorA_en =3;
 
-//infrared sensor
-const int IRpin = 4; 
+//motor B
+const int motorB_1 = 4; //in1 
+const int motorB_2 = 5; //in2 
+const int motorB_en = 6;
+
+//infrared sensors
+const int IR1pin = A0; //keep in an analog port since it didnt work in the other ones 
+const int IR2pin = A1; 
+
+
+
+//reflectance sensor 
+const int rs_1 = A2;
+const int rs_2 = A3;
+const int rs_3 = A4;
+const int rs_4 = A5;
 
 
 
@@ -21,19 +35,24 @@ void setup() {
 
 
   //infrared stuff
-  
+  pinMode(IRpin, INPUT); // IR Sensor pin INPUT
 
-  //motor A setup
-  pinMode(motorA_en, OUTPUT); //motor speed 0 to 255
+
+  // //motor A setup
+  pinMode(motorA_en, OUTPUT); //motor speed 0 to 255, however max it at 100 to not fry 
   pinMode(motorA_1, OUTPUT);
   pinMode(motorA_2, OUTPUT);
-  // Set initial rotation direction
-  //to go in a direction they have to be opposing, swithcing which one is high and low makes it go forward or backward 
+  // // Set initial rotation direction
+  // //to go in a direction they have to be opposing, swithcing which one is high and low makes it go forward or backward 
   digitalWrite(motorA_1, LOW);
   digitalWrite(motorA_2, HIGH);
 
 
-   
+  //motor B setup
+
+  //reflexive stuff
+  pinMode(rs_1, INPUT); //gives a number from 0 to 1023, lower means closer to white, higher means closer to black
+  pinMode(control_rs_1, OUTPUT); //if high it turns on the led to get the reflexive info
 
 
 
@@ -48,28 +67,41 @@ void loop() {
 
 
   //infrared
+
   //this gets me a distance for which we can judge or next move 
-  float volts = analogRead(IRpin)*0.0048828125;   
-  float distance = 65*pow(volts, -1.10);   
-  Serial.print(distance);     
-  Serial.println(" cm");
-  delay(100); 
+  float distance = analogRead(IRpin);   
+  Serial.println(distance);     
+  delay(1000); 
+
+  
+  //reflexive
+  digitalWrite(control_rs_1, HIGH);
+  float light = analogRead(rs_1);
+  Serial.println(light);     
+  delay(250); 
+
+
 
   //wheel dirrection
-  //If button is pressed - change rotation direction
   //make it work with sensors instead 
-  if (pressed == true  & rotDirection == 0) {
+  if (true) {
     digitalWrite(motorA_1, HIGH);
     digitalWrite(motorA_2, LOW);
-    rotDirection = 1;
+    analogWrite(motorA_en, 100);
+    //rotDirection = 1;
     delay(20);
+    Serial.println("rotated true");
   }
+
+
   // If button is pressed - change rotation direction
-  if (pressed == false & rotDirection == 1) {
+  if (false) {
     digitalWrite(motorA_1, LOW);
     digitalWrite(motorA_2, HIGH);
-    rotDirection = 0;
+    analogWrite(motorA_en, 100);
+    //rotDirection = 0;
     delay(20);
+    Serial.println("rotated false");
   }
 
 
